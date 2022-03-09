f68257f36f1a34bf1bf8867f1a1548af632aea18
Austin <awoods7753@gmail.com>
2022-03-09 17:33:17 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+    let values = {
+        name: name,
+        email: email}
+        const configurationObject = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                 Accept: "application/json",
+            },
+            body: JSON.stringify(values),
+    };
+fetch("http://localhost:3000/users", configurationObject)
+.then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    document.body.innerHTML = object.id
+  })
+  .catch(function (error) {
+    document.body.innerHTML = error.message
+  });
+
+}
